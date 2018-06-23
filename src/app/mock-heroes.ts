e6db9cf1a12d3cc1f31d8cd52630ed90e68ff038
Ashley Winnop <arwinnop@gmail.com>
2018-06-23 16:11:56 -0600

    displaying list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 2, name: 'Aphrodite' },
+  { id: 3, name: 'Athena' },
+  { id: 4, name: 'Demeter' },
+  { id: 5, name: 'Hera' },
+  { id: 6, name: 'Hestia' },
+  { id: 7, name: 'Apollo' },
+  { id: 8, name: 'Dionysus' },
+  { id: 9, name: 'Hades' },
+  { id: 10, name: 'Hermes' },
+  { id: 11, name: 'Poseidon' },
+  { id: 12, name: 'Zeus' }
+];
